c3765043001a53c11a07ac1f69d4d593b855be09
Rajmund <rajmund.hutar@gmail.com>
2015-04-26 13:15:25 +0200

    Initital delete commit

--- css/styles.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "../php.php";
-
-header("Content-type: text/css", true);
-
-$less = new lessc;
-echo $less->compileFile("./styles.less");
--- historie2006.php
+++
@@ -1,8 +0,0 @@
-<?
-require("./php.php");
-head("Hučka 2006",True,0,"historie");
-?>
-Tato krásná akce začala pro diváky něco po 4 hodině odpolední 9 prosince. Pro mně osobně už tak o měsíc dříve na schůzkách v čajovně. Jako každý rok jsem se na hučku strašně těšil. A doufám, že i ostatní se těšili. Přípravě jsme snad všichni věnovali maximum. A tak si myslím, že i přes malé nedostatky se nám Hučka povedla. A že byla super. Napsal bych sem pár řádků z divadel, ale jelikož jsem viděl jen kus divadla 4. Znojmo, kousek od Žížalových vodních skautů a útržky z ostatních tak myslím, že divadla mají rok od roku čím dál větší úroveň a zvedá se i kvalita hereckých výkonů. Ač třeba nám se letos nějak nepodvedlo ozvučit sál tak myslím, že divadla byla dobrá a tanec s hudbou byla taková milá hezká veselá tečka. No toť asi ode mně vše k Hučce, snad bych jen poděkoval všem organizátorům za čas a energii vynaložené k Hučce. Tak vám všem VELKÝ DÍK.. zdraví vás Tydýt :-8
-
-
-<? foot(True);?>
